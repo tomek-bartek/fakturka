627cabf03c4ace8d3aabf687eb444aeff5b6cabc
tomek-bartek <tomskibit1@gmail.com>
2022-06-18 13:02:10 +0200

    Dodany package Company i przeniesiony controller

--- src/main/java/com/example/fakturka/Company/CompanyController.java
+++ src/main/java/com/example/fakturka/Company/CompanyController.java
@@ -1,4 +1,4 @@
-package com.example.fakturka;
+package com.example.fakturka.Company;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
